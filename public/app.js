88df1b4efbd6808bcf55ac777e600379556896c6
jp <jp@example.com>
2026-08-05 22:31:24 +0900

    style: Remove modal-desc text from budget setting modal and bump to v2.0

--- public/app.js
+++ public/app.js
@@ -7,7 +7,7 @@
   'use strict';
 
   // --- Constants & Default Data ---
-  const APP_VERSION = 'v1.9 (2026-08-05)';
+  const APP_VERSION = 'v2.0 (2026-08-05)';
   const DEFAULT_BUDGET_EMART = 60000;
   const DEFAULT_BUDGET_COSTCO = 300000;
 
